027250a9b576ef2c7233f18298b557d8a2425a84
mgrztwnk5@gmail.com <mgrztwnk5@gmail.com>
2023-06-21 22:51:58 +0200

    M

--- AnimaFB/server/src/types/user.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose'
-
-export interface IUser extends Document {
-    age: number
-    gender: string
-    sayYesNo: string
-    animationType: Array<string>
-    status: boolean
-}
