d16de02ec60b842eab4ec2d2734b7223dc21036b
mgrztwnk5@gmail.com <mgrztwnk5@gmail.com>
2023-06-26 23:27:34 +0200

    Iniatial change

--- client/src/components/animationsObjects/AnimationThreeD.tsx
+++ client/src/components/animationsObjects/AnimationThreeD.tsx
@@ -1,78 +1,64 @@
+import React, { useRef } from 'react';
+import { useFrame,  } from '@react-three/fiber';
 import * as THREE from 'three';
-import { useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
-import React, { useEffect, useState } from 'react';
-import { Material } from 'three';
+import { generateRandomAnimation } from '../../library/utils/functions';
 import { AnimationMotionProps } from '../types/Animation';
 
-export default function RandomMove3D({ updatePositions }: AnimationMotionProps) {
-  const mesh = useRef<THREE.Mesh<THREE.BufferGeometry, Material | Material[]>>(null);
-  const [x, setX] = useState(Math.random() * 10 - 3);
-  const [y, setY] = useState(Math.random() * 10 - 3);
-  const [z, setZ] = useState(0);
-  const [direction, setDirection] = useState(1); // Kierunek poruszania się piłki (1 - prawo, -1 - lewo)
+let acceleration = 0.05;
+let bounce_distance = generateRandomAnimation(0,5);
+let bottom_position_y = 0;
+let time_step = 0.1;
+// time_counter  jest obliczany jako czas, w którym kulka osiągnęła górną pozycję
+// to jest po prostu obliczane za pomocą wzoru s = (1/2)gt*t, co ma miejsce w przypadku upuszczenia piłki z górnej pozycji
+//od góry do dołu
+let time_counter = Math.sqrt((-bounce_distance * 2) / -acceleration);
 
-  // Zdefiniuj losową pozycję dla kuli
-  const randomPosition = () => {
-    return new THREE.Vector3(x, y, z);
-  };
+//od dołu do góry
+// let time_counter = Math.sqrt((bounce_distance * 2) / acceleration);
+let initial_speed = acceleration * time_counter;
+export default function SphereMove({ updatePositions }: AnimationMotionProps ) {
+  const mesh = useRef<THREE.Mesh | null>(null);
+  
+  var num = generateRandomAnimation(1, 15);
 
-  // Zdefiniuj losową prędkość dla kuli
-  const randomSpeed = () => {
-    return Math.random() * 0.1;
-  };
-
-  // Ustawienie początkowego położenia i prędkości kuli
-  let position = randomPosition();
-  let speed = randomSpeed();
-
- 
-
-  // Użycie hook'a useFrame, aby aktualizować pozycję sfery co klatkę
   useFrame(() => {
-    if (mesh.current) {
-      // Uaktualnienie pozycji kuli na podstawie jej aktualnej pozycji, prędkości i czasu, który upłynął od ostatniej klatki
-      mesh.current.position.x += speed * direction * (position.x - mesh.current.position.x);
-      mesh.current.position.y += speed * direction * (position.y - mesh.current.position.y);
-      mesh.current.position.z += speed * direction * (position.z - mesh.current.position.z);
-
-      // Jeśli kula jest wystarczająco blisko pozycji docelowej, wybierz dla niej nową losową pozycję, prędkość i kierunek ruchu
-      if (mesh.current.position.distanceTo(position) < 0.1) {
-        setDirection(Math.random() < 0.5 ? 1 : -1); // Losowo ustawiamy kierunek na prawo lub lewo
-
-        if (direction === 1) {
-          // Poruszanie w prawo
-          setX(Math.random() * 10 - 3);
-        } else {
-          // Poruszanie w lewo
-          setX(-(Math.random() * 10 - 3));
-        }
-
-        setY(Math.random() * 10 - 3);
-        setZ(0);
-
-        position = randomPosition();
-        speed = randomSpeed();
+    if(mesh.current){
+    if (num % 2) {
+      if (mesh.current.position.x < bottom_position_y) {
+        time_counter = 0;
       }
+      // console.log(mesh.current.position.y);
+      mesh.current.position.x =
+        bottom_position_y +
+        initial_speed * time_counter -
+        0.5 * acceleration * time_counter * time_counter;
+      // advance time
+      time_counter += time_step;
+    } else {
+      if (mesh.current.position.y < bottom_position_y) {
+        time_counter = 0;
+      }
+      // console.log(mesh.current.position.y);
+      mesh.current.position.y =
+        bottom_position_y +
+        initial_speed * time_counter -
+        0.5 * acceleration * time_counter * time_counter;
+      // advance time
+      time_counter += time_step;
     }
+  }
   });
-
-  useEffect(() => {
-    updatePositions([x, y, z], [speed], "Random", "infinite");
-  }, []);
-
+  // updatePositions([],[initial_speed],'linear',String(bounce_distance))
   return (
-    <mesh>
-      <mesh ref={mesh}>
-        <sphereGeometry attach="geometry" args={[1, 16, 16]} />
-        <meshStandardMaterial
-          attach="material"
-          color="gray"
-          transparent
-          roughness={0.1}
-          metalness={0.1}
-        />
-      </mesh>
+    <mesh ref={mesh}>
+      <sphereGeometry attach="geometry" args={[1, 16, 16]} />
+      <meshStandardMaterial
+        attach="material"
+        color="gray"
+        transparent
+        roughness={0.1}
+        metalness={0.1}
+      />
     </mesh>
   );
 }
