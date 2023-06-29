0b96cdfc7690283e8626b0c810a93b6528c4c152
mgrztwnk5@gmail.com <mgrztwnk5@gmail.com>
2023-06-29 12:57:51 +0200

    Add server/dist/js/models/addanimations.js

---
+++ server/dist/js/models/addanimations.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.aaddanimationSchema = void 0;
+const mongoose_1 = require("mongoose");
+exports.aaddanimationSchema = new mongoose_1.Schema({
+    id: {
+        type: String,
+    },
+    user_Id: {
+        type: String,
+    },
+    position: {
+        type: Array,
+    },
+    image: {
+        type: String,
+    },
+    movement: {
+        type: Array,
+    },
+    speed: {
+        type: Array,
+    },
+    distance: {
+        type: Array,
+    },
+    status: {
+        type: Boolean,
+    },
+}, { timestamps: true });
+exports.default = (0, mongoose_1.model)('AddAnimation', exports.aaddanimationSchema);
